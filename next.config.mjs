66a205fffa8a255f19d47bdb75d7090e66566e47
khilyao <sfwew19@gmail.com>
2024-06-28 16:20:36 +0300

    change next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true,
-  },
-  output: "export",
-  reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
